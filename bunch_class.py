a031c2331bf24db1a3452481f332cdf2816454b6
Michelle Rex <162601741+Michelle-Rex@users.noreply.github.com>
2026-01-04 17:06:44 +0000

    merged cylinder class into bunch class due to import error and changed bunch init

--- bunch_class.py
+++ bunch_class.py
@@ -1,14 +1,36 @@
-from cylinder_class.py import Cylinder
+#from cylinder_class.py import Cylinder
 import numpy as np
+
+class Cylinder:
+    def __init__(self, radius, weight):
+        self.radius = radius
+        self.weight = weight
+        self.x = 0
+        self.y = 0
+        self.is_placed = False
+
+    def set_pos(self, x, y):
+        self.x = x
+        self.y = y
+        self.is_placed = True
+    
+    def get_origin_distance(self):
+        return np.sqrt(self.x**2 + self.y**2)
+    
+    def containing_radius(self):
+        return self.get_origin_distance + self.radius
+    
+    def distance_to_other(self, other):
+        return np.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+
+    def check_overlap(self, other):
+        distance = self.distance_to_other(other)
+        return distance < (self.radius + other.radius + 0.01)
 
 class Bunch:
     #manages the collection of cylinders and the placement aglorithms
     def __init__(self, radii, weight):
-        self.cylinders = [
-            Cylinder(r, w)
-            for r in radii 
-            for w in weight
-        ]
+        self.cylinders = [Cylinder(radii[i], weight[i]) for i in (0, len(radii)-1)]
         self.radii = radii
         self.weight = weight
 
@@ -51,3 +73,9 @@ class Bunch:
     def ordered_place(self):
         for c in self.cylinders:
             pass
+
+#testing
+
+test_bunch = Bunch([10, 6], [10, 8])
+for b in test_bunch.cylinders:
+    print(b.weight)
