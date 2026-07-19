5a67d25616b622fdcaf3edbdc7914e58d92ef662
DinosaurusRexy <162601741+DinosaurusRexy@users.noreply.github.com>
2026-07-19 19:24:08 +0100

    Start making draw function

--- bunch_class.py
+++ bunch_class.py
@@ -54,14 +54,55 @@ class Bunch:
                 c1 = placed[i]
                 c2 = placed [j]
 
-    def find_tangent_positions(self, c1, c2, new):
+                positions = self.find_tangent_positions(c1, c2, new_cylinder)
+                for x,y in positions: 
+                    temp_cyl = cyl.Cylinder(new_cylinder.radius, new_cylinder.weight)
+                    temp_cyl.set_pos(x, y)
+
+                    valid = True
+
+                    for other in placed:
+                        if other != c1 and other != c2:
+                            if temp_cyl.check_overlap(other):
+                                valid = False
+                                break
+
+                    if valid:
+                        dist_from_origin = np.sqrt(x**2 + y**2)
+                        open_points.append((x, y, dist_from_origin))
+
+        return open_points
+
+    def find_tangent_positions(self, c1, c2, new_cylinder):
          #find where the new circle can be placed to tangent with the previous 2 (uses circle-circle intersection --not entirely sure how the geometry works)
-        r1 = c1.radius + new.radius
-        r2 = c2.radius + new.radius
+        r1 = c1.radius + new_cylinder.radius
+        r2 = c2.radius + new_cylinder.radius
         d = c1.distance_to_other(c2)
 
         if d > r1 + r2 or d < abs(r1 - r2) or d ==0:
-            pass
+            return []
+        
+        a = (r1**2 - r2**2 +d**2)/ (2*d)
+        h = np.sqrt(max(0, r1**2 - a**2))
+
+        px = c1.x + a * (c2.x - c1.x) / d
+        py = c1.y + a * (c2.y - c1.y) / d
+
+        positions = []
+
+        if h > 0.01:
+            positions.append((
+                px + h * (c2.y - c1.y) / d,
+                py - h * (c2.x - c1.x) / d
+            ))
+            positions.append((
+                px - h * (c2.y - c1.y) / d,
+                py + h * (c2.x - c1.x) / d
+            ))
+        else:
+            positions.append((px, py))
+
+        return positions
         
 
 
@@ -90,5 +131,16 @@ class Bunch:
 
             if open_points:
                 pass
+
+    def draw(self, title = "Cylinder Placement", show_open_points = False):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        containing_radius = self.get_containing_radius()
+        container = plt.Rectangle((0,0), width = self.rec_width, height = self.rec_len, fill = False, edgecolour = "black", linewidth = 2, label = "Cargo Container")
+        
+        ax.add_patch(container)
+
+        if show_open_points and len([c for c in self.cylinders if c.is_placed]) < len(self.cylinders):
+            next_cylinder = [c for c in self.cylinders if not c.is_placed][0]
+            open_points = self.find_open_points(next_cylinder)
 
 #testing
