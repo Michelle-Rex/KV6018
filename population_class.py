22f700e5111622f05057e02c2e8a7f83624cf6a8
DinosaurusRexy <162601741+DinosaurusRexy@users.noreply.github.com>
2026-07-28 13:44:12 +0100

    added imports to population class.py

--- population_class.py
+++ population_class.py
@@ -1,3 +1,9 @@
+import bunch_class as bunch
+import cylinder_class as cyl
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Population:
+    
     def __init__(self, pop_size, mutation_rate, population, max_generations):
         pass
